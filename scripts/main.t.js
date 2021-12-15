1ffeeffc6da53862b2da0e6019e1c25918d7c068
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-12-15 11:33:38 +0900

    add: temp js temp now temp
    Signed-off-by: halka9000stg <csta19097@g.nihon-u.ac.jp>

--- scripts/main.t.js
+++ scripts/main.t.js
@@ -1,5 +1,11 @@
 //import $ from "https://code.jquery.com/jquery-3.5.1.min.js";
 // export for others scripts to use
+let act = false;
+let cout = 0;
+console.log("Web page was loaded. \nNow is " + (act ? "active" : "not active") + ".\non " + cout + " times of toggle, and " + Math.floor((cout + 1) / 2) + " times of set.");
 $(":root").click(function () {
   $(this).toggleClass('active');
+  act = !act;
+  cout++;
+  console.log("Menu Button was toggled. \nNow is " + (act ? "active" : "not active") + ".\non " + cout + " times of toggle, and " + Math.floor((cout + 1) / 2) + " times of set.");
 });
